f8a639909c105ef7631dcbbb5fad3d0937734a39
StackOverflowIsBetterThanAnyAI <michael.muenzenhofer@gmail.com>
2024-07-21 23:46:04 +0200

    rumble 1988 and 2019

--- src/script.js
+++ src/script.js
@@ -1,7 +1,10 @@
-function displayCountdown(number) {
-    const countdownElements = document.getElementsByClassName('countdown-10')
-    const displayNumber =
-        parseInt(number) === 10 ? number : `0${parseInt(number)}`
+function displayCountdown(number, maxNumber, className) {
+    const countdownElements = document.getElementsByClassName(className)
+    const displayNumber = /leadingZero/.test(className)
+        ? parseInt(number) >= 10
+            ? number
+            : `0${parseInt(number)}`
+        : number
 
     for (let i = 0; i < countdownElements.length; i++) {
         countdownElements[i].innerHTML = displayNumber
@@ -9,11 +12,15 @@ function displayCountdown(number) {
 
     const timer = setTimeout(
         () =>
-            number === 0 ? displayCountdown(10) : displayCountdown(number - 1),
+            number === 0
+                ? displayCountdown(maxNumber, maxNumber, className)
+                : displayCountdown(number - 1, maxNumber, className),
         1000
     )
 
     return () => clearTimeout(timer)
 }
 
-displayCountdown(10)
+displayCountdown(10, 10, 'countdown-10')
+displayCountdown(10, 10, 'countdown-10-leadingZero')
+displayCountdown(12, 12, 'countdown-12-leadingZero')
